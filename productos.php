2eef07efaf02b2272629226a121ef98c159c27b2
simon <gilbertoarmenta656@gmail.com>
2018-07-28 05:50:37 -0400

    Create productos.php

---
+++ productos.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+<meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	 <meta name="description" content="Sistema informatico Online">
+    <link rel="icon" href="https://materializecss.com/images/favicon/favicon-32x32.png" sizes="32x32">
+    <meta name="theme-color" content="black">
+	<title>Productos</title>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style_home.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+</head>
+
+<body>
+<nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo">Productos</a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="home.html">Inicio</a></li>
+        <li><a href="#">Inventario</a></li>
+        <li><a href="#">Ventas</a></li>
+        <li><a href="#">Manual de usuario</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="home.html">Inicio</a></li>
+    <li><a href="#">Inventario</a></li>
+    <li><a href="#">Ventas</a></li>
+    <li><a href="#">Manual de usuario</a></li>
+  </ul>
+          
+
+
+<div class="container">
+<div class="row">
+ <br> 
+   <table>
+        <thead>
+          <tr>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Precio</th>
+              <th>Catalogo</th>
+              <th>Sucursal</th>
+              <th>Cantidad en inventario</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>01</td>
+            <td>LG smart TV 20 pulgadas</td>
+            <td>$200</td>
+             <td>TV smart</td>
+            <td>Centro</td>
+             <td>20</td>
+
+          </tr>
+          <tr>
+          <td>02</td>
+            <td>LG smart TV 20 pulgadas</td>
+            <td>$200</td>
+             <td>TV smart</td>
+            <td>Centro</td>
+             <td>20</td>
+
+
+          </tr>
+          <tr>
+         <td>03</td>
+            <td>LG smart TV 20 pulgadas</td>
+            <td>$200</td>
+             <td>TV smart</td>
+            <td>Centro</td>
+             <td>20</td>
+
+          </tr>
+        </tbody>
+      </table>
+            
+
+</div>
+</div>
+
+
+
+<br><br><br>
+ <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Sistema online</h5>
+                <p class="grey-text text-lighten-4">Sistema online hoop - Mexico</p>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2018 Sistema online
+            <a target="_black" class="grey-text text-lighten-4 right" href="https://gilbertoquinteroa.github.io/MyWork/">Desarrollador por</a>
+            </div>
+          </div>
+        </footer>
+            
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+<script>
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+</script>
+</body>
+</html>
+
