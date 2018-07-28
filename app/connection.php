86e8c1387fc6adbcc44e22de9e32bf7fa1f98121
simon <gilbertoarmenta656@gmail.com>
2018-07-28 06:02:43 -0400

    Update connection.php

--- app/connection.php
+++ app/connection.php
@@ -1,22 +1,22 @@
 <?php 
 
-$localhost = "127.0.0.1"; 
-$username = "root"; 
-$password = "quintero0123A"; 
+//$localhost = "127.0.0.1"; 
+//$username = "root"; 
+//$password = "quintero0123A"; 
 
 //bd_system_online
-$dbname = "bd_system_online"; 
+//$dbname = "bd_system_online"; 
  
 
 // create connection 
-$connect = new mysqli($localhost, $username, $password, $dbname); 
+//$connect = new mysqli($localhost, $username, $password, $dbname); 
  
 // check connection 
-if($connect->connect_error) {
-    die("conexion fallida : " . $connect->connect_error);
-} else {
-    echo "<script>alert('se conecto');</script>";
-}
+//if($connect->connect_error) {
+//    die("conexion fallida : " . $connect->connect_error);
+////} else {
+ //   echo "<script>alert('se conecto');</script>";
+//}
  
 ?>
 
